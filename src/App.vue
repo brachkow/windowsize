9af7c18dfcc81869d65a7bd6f068b90f0566c459
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-01-28 16:24:59 +0100

    footer added

--- src/App.vue
+++ src/App.vue
@@ -24,53 +24,71 @@
 </script>
 
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-        <th>width</th>
-        <th>height</th>
-        <th>aspect ratio</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Device screen physical resolution</td>
-        <td>{{ deviceScreen?.width }}</td>
-        <td>{{ deviceScreen?.height }}</td>
-        <td>{{ deviceScreen?.aspectRatio }}</td>
-      </tr>
-      <tr>
-        <td>Device screen resolution</td>
-        <td>{{ deviceViewport?.width }}</td>
-        <td>{{ deviceViewport?.height }}</td>
-        <td>{{ deviceViewport?.aspectRatio }}</td>
-      </tr>
-      <tr>
-        <td>Browser viewport resolution</td>
-        <td>{{ browserViewport?.width }}</td>
-        <td>{{ browserViewport?.height }}</td>
-        <td>{{ browserViewport?.aspectRatio }}</td>
-      </tr>
-      <tr>
-        <td>Browser window size</td>
-        <td>{{ browserWindow?.width }}</td>
-        <td>{{ browserWindow?.height }}</td>
-        <td>{{ browserWindow?.aspectRatio }}</td>
-      </tr>
-    </tbody>
-    <tfoot>
-      <tr>
-        <td colspan="4">all values are in px</td>
-      </tr>
-    </tfoot>
-  </table>
+  <main>
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>width</th>
+          <th>height</th>
+          <th>aspect ratio</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Device screen physical resolution</td>
+          <td>{{ deviceScreen?.width }}</td>
+          <td>{{ deviceScreen?.height }}</td>
+          <td>{{ deviceScreen?.aspectRatio }}</td>
+        </tr>
+        <tr>
+          <td>Device screen resolution</td>
+          <td>{{ deviceViewport?.width }}</td>
+          <td>{{ deviceViewport?.height }}</td>
+          <td>{{ deviceViewport?.aspectRatio }}</td>
+        </tr>
+        <tr>
+          <td>Browser viewport resolution</td>
+          <td>{{ browserViewport?.width }}</td>
+          <td>{{ browserViewport?.height }}</td>
+          <td>{{ browserViewport?.aspectRatio }}</td>
+        </tr>
+        <tr>
+          <td>Browser window size</td>
+          <td>{{ browserWindow?.width }}</td>
+          <td>{{ browserWindow?.height }}</td>
+          <td>{{ browserWindow?.aspectRatio }}</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colspan="4">all values are in px</td>
+        </tr>
+      </tfoot>
+    </table>
+  </main>
+  <footer>
+    <div>Made by <a href="https://brachkow.com">Timur Brachkow</a></div>
+    <div><a href="https://github.com/brachkow/windowsize">Github</a></div>
+  </footer>
 </template>
 
 <style lang="scss">
   html {
     font-size: 14px;
     font-family: monospace;
+  }
+
+  body {
+    margin: 0;
+  }
+
+  #app {
+    --body-padding: 16px;
+    display: grid;
+    grid-template-rows: 1fr auto;
+    padding: var(--body-padding);
+    min-height: calc(100vh - var(--body-padding) * 2);
   }
 
   @media (prefers-color-scheme: dark) {
@@ -112,5 +130,10 @@
       padding-left: 8px;
       text-align: right;
     }
+  }
+
+  footer {
+    display: flex;
+    justify-content: space-between;
   }
 </style>
