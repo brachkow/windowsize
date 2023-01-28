19c54856cac8558f8044afca0ba59523cf465e86
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-01-28 16:39:53 +0100

    refactor gcd

--- src/utils/gcd.ts
+++ src/utils/gcd.ts
@@ -1,9 +1,3 @@
-const gcd = (a: number, b: number): number => {
-  if (!b) {
-    return a;
-  }
-
-  return gcd(b, a % b);
-};
+const gcd = (a: number, b: number): number => (!b ? a : gcd(b, a % b));
 
 export default gcd;
