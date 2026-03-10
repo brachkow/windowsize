4f169c1b545a5b6dc292aaa26d9390af7fe0d06d
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2026-03-10 02:24:51 +0100

    Use openpanel

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import { inject } from '@vercel/analytics';
+import { OpenPanel } from '@openpanel/web';
 
-inject();
+// eslint-disable-next-line no-new
+new OpenPanel({
+  apiUrl: 'https://opdashboard-o8c4s0cg0ocoo08okoock40k.brachkow.com/api',
+  clientId: 'cd0deb0a-11ef-461e-8134-c8d638783bd6',
+  trackScreenViews: true,
+  trackOutgoingLinks: true,
+  trackAttributes: true,
+});
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 createApp(App).mount('#app');
